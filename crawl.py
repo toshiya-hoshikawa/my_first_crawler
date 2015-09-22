3088b1bf17cddddc0000ba0c2588326669cdfd93
Toshiya Hoshikawa <tsyhskw@gmail.com>
2015-09-22 18:00:23 +0900

    added Crawler class

--- crawl.py
+++ crawl.py
@@ -47,3 +47,59 @@ class Retriever(object):
         parser.close()
         return parser.anchorlist
 
+class Crawler(object):
+    count = 0
+
+    def __init__(self, url):
+        self.q = [url]
+        self.seen = set()
+        parsed = urlparse.urlparse(url)
+        host = parsed.netloc.split('@')[-1].split(':')[0]
+        self.dom = '.'.join(host.split('.')[-2:])
+
+    def get_page(self, url, media=False):
+        'Download page & parse links, add to queue if never seen'
+        r = Retriever(url)
+        fname = r.download()[0]
+        if fname[0] == '*':
+            print fname, '...skipping parse'
+            return
+        Crawler.count += 1
+        print '\n(', Crawler.count, ')'
+        print 'URL:', url
+        print 'FILE:', fname
+        self.seen.add(url)
+        ftype = os.path.splitext(fname)[1]
+        if ftype not in ('.htm', '.html'):
+            return
+
+        for link in r.parse_links():
+            if link.startswith('mailto:'):
+                print '... discarded, mailto link'
+                continue
+            if not media:
+                ftype = os.path.splitext(link)[1]
+                if ftype in ('.mp3', '.mp4', '.m4v', '.wav'):
+                    print '... discarded, media file'
+                    continue
+            if not link.startswith('http://'):
+                link = urlparse.urljoin(url, link)
+            print '*', link
+            if link not in self.seen:
+                if self.dom not in link:
+                    print '... discarded, not in domain'
+                else:
+                    if link not in self.q:
+                        self.q.append(link)
+                        print '... new, added to Q'
+                    else:
+                        print '... discarded, already in Q'
+            else:
+                print '... discarded, already processed'
+
+    def go(self, media=False):
+        'Process next page in queue (if any)'
+        while self.q:
+            url = self.q.pop()
+            self.get_page(url, media)
+
